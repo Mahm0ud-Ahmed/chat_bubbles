346605916738843ff703251df53f6576b7f12c56
Mahmoud Ahmed <developermahmoud91@gmail.com>
2024-10-11 01:15:08 +0300

    replace to stream builder

--- lib/src/presentation/view/pages/pages/home/widget/users_list.dart
+++ lib/src/presentation/view/pages/pages/home/widget/users_list.dart
@@ -1,5 +1,3 @@
-import 'dart:async';
-
 import 'package:chat_bubbles/src/core/utils/extension.dart';
 import 'package:flutter/material.dart';
 
@@ -11,88 +9,61 @@ import '../../../../common/image_widget.dart';
 import '../../../../common/text_widget.dart';
 import '../home_cubit.dart';
 
-class UsersList extends StatefulWidget {
+class UsersList extends StatelessWidget {
   final HomeCubit<UserModel> chat;
   const UsersList({super.key, required this.chat});
 
   @override
-  State<UsersList> createState() => _UsersListState();
-}
-
-class _UsersListState extends State<UsersList> {
-  DataState<List<UserModel>>? data;
-  String? error;
-  late final StreamSubscription<DataState<List<UserModel>>> stream;
-
-  @override
-  void initState() {
-    super.initState();
-    stream = widget.chat.getUsers().listen(
-      (event) {
-        setState(() {
-          data = event;
-          error = null;
-        });
-      },
-      onError: (e) {
-        setState(() {
-          error = e.toString();
-        });
-      },
-    );
-  }
-
-  @override
-  void dispose() {
-    stream.cancel();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    if (error != null) {
-      return Center(child: TextWidget(text: error!));
-    } else if (data != null) {
-      return data!.when(
-        success: (success) {
-          return ListView.builder(
-            itemCount: success.length,
-            itemBuilder: (context, index) {
-              final user = success[index];
-              return Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: ListTile(
-                  leading: Stack(
-                    children: [
-                      ClipOval(
-                        child: ImageWidget(image: user.avatar, width: 50, height: 50),
-                      ),
-                      if (user.onlineStatus!)
-                        Positioned(
-                          bottom: 0,
-                          right: 8,
-                          child: Container(
-                            width: 10,
-                            height: 10,
-                            decoration: BoxDecoration(
-                              color: ThemeColor.successColor.color,
-                              shape: BoxShape.circle,
+    return StreamBuilder<DataState<List<UserModel>>>(
+      stream: chat.getUsers(),
+      builder: (BuildContext context, AsyncSnapshot<DataState<List<UserModel>>> snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return AppIndicator();
+        } else if (snapshot.connectionState == ConnectionState.done && snapshot.hasError) {
+          return TextWidget(text: snapshot.error.toString());
+        }
+        final data = snapshot.data;
+        return data!.when(
+          success: (data) {
+            return ListView.builder(
+              itemCount: data.length,
+              itemBuilder: (context, index) {
+                final user = data[index];
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ListTile(
+                    leading: Stack(
+                      children: [
+                        ClipOval(
+                          child: ImageWidget(image: user.avatar, width: 50, height: 50),
+                        ),
+                        if (user.onlineStatus!)
+                          Positioned(
+                            bottom: 0,
+                            right: 8,
+                            child: Container(
+                              width: 10,
+                              height: 10,
+                              decoration: BoxDecoration(
+                                color: ThemeColor.successColor.color,
+                                shape: BoxShape.circle,
+                              ),
                             ),
                           ),
-                        ),
-                    ],
+                      ],
+                    ),
+                    title: TextWidget(text: user.userName),
+                    subtitle: TextWidget(text: user.email),
+                    onTap: () => context.nextNamed(AppRoute.chat.route, argument: user.uid),
                   ),
-                  title: TextWidget(text: user.userName),
-                  subtitle: TextWidget(text: user.email),
-                  onTap: () => context.nextNamed(AppRoute.chat.route, argument: user.uid),
-                ),
-              );
-            },
-          );
-        },
-        failure: (value) => TextWidget(text: value.statusMessage ?? ''),
-      );
-    }
-    return const AppIndicator();
+                );
+              },
+            );
+          },
+          failure: (value) => TextWidget(text: value.statusMessage ?? ''),
+        );
+      },
+    );
   }
 }
